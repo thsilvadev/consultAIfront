2c0c3886e3f98ff88d65f5e29543076c75d0a12a
thsilvadev <thsilva.developer@gmail.com>
2025-02-05 16:45:27 -0400

    resetting to deploy on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/consultAIfront/', // Deve ser EXATAMENTE o nome do repositório no GitHub
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
+  base: './', // Deve ser EXATAMENTE o nome do repositório no GitHub
+
 });
